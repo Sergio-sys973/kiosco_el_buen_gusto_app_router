297a8a0aa73b3608bdb9b96d2e9316f804ed55f2
Sergio-sys973 <detallesstp@gmail.com>
2025-06-05 15:39:02 -0400

    fix

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const now = new Date();
     const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
